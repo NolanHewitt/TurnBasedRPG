3916f0646fce0b22a0e5132e3c28b7e792ca93f7
Nolan Hewitt <nolanhewitt@aol.com>
2020-01-25 03:03:16 -0600

    added transactional functions

--- public/script/script.js
+++ public/script/script.js
@@ -12,6 +12,85 @@ let lvl3 = localStorage.getItem("lvlThree");
 let exp1 = localStorage.getItem("expOne");
 let exp2 = localStorage.getItem("expTwo");
 let exp3 = localStorage.getItem("lvlThree");
+//Money for each file
+let money1 = localStorage.getItem("money1");
+let money2 = localStorage.getItem("money2");
+let money3 = localStorage.getItem("money3");
+//Weapon for each file
+let weapon1 = localStorage.getItem("weapon1");
+let weapon2 = localStorage.getItem("weapon2");
+let weapon3 = localStorage.getItem("weapon3");
+//Weapon attack for each file
+let weaponAtk1 = localStorage.getItem("weaponAtk1");
+let weaponAtk2 = localStorage.getItem("weaponAtk2");
+let weaponAtk3 = localStorage.getItem("weaponAtk3");
+//Weapon value for each file
+let weaponValue1 = localStorage.getItem("weaponValue1");
+let weaponValue2 = localStorage.getItem("weaponValue2");
+let weaponValue3 = localStorage.getItem("weaponValue3");
+//Armor for each file
+let armor1 = localStorage.getItem("armor1");
+let armor2 = localStorage.getItem("armor2");
+let armor3 = localStorage.getItem("armor3");
+//Armor defense for each file
+let armorDef1 = localStorage.getItem("armorDef1");
+let armorDef2 = localStorage.getItem("armorDef2");
+let armorDef3 = localStorage.getItem("armorDef3");
+//Armor value for each file
+let armorValue1 = localStorage.getItem("armorValue1");
+let armorValue2 = localStorage.getItem("armorValue2");
+let armorValue3 = localStorage.getItem("armorValue3");
+//Inventory for each file
+let inventory1 = localStorage.getItem("inventory1");
+let inventory2 = localStorage.getItem("inventory2");
+let inventory3 = localStorage.getItem("inventory3");
+//Inventory value for each file
+let inventoryValue1 = localStorage.getItem("inventoryValue1");
+let inventoryValue2 = localStorage.getItem("inventoryValue2");
+let inventoryValue3 = localStorage.getItem("inventoryValue3");
+//Arrays for updating inventory
+let stuff1 = [];
+let stuff2 = [];
+let stuff3 = [];
+//Arrays for updating inventory value
+let stuffValue1 = [];
+let stuffValue2 = [];
+let stuffValue3 = [];
+//Variables for holding inventory parsed from local storage
+let stuffParse1;
+let stuffParse2;
+let stuffParse3;
+//Variables for holding inventory values parsed from local storage
+let stuffValueParse1;
+let stuffValueParse2;
+let stuffValueParse3;
+
+//Process for adding an item to inventory1 as well as its corosponding value. Don't use this until after intro sets inventory to empty array.
+// Check to see if user has enough money in local storage if this is a transaction
+// stuff1 = JSON.parse(localStorage.getItem("inventory1"));
+// stuffValue1 = JSON.parse(localStorage.getItem("inventoryValue1"));
+// stuff1.push("item2");
+// stuffValue1.push("itemValue2");
+// localStorage.setItem("inventory1", JSON.stringify(stuff1));
+// localStorage.setItem("inventoryValue1", JSON.stringify(stuffValue1));
+// stuffParse1 = JSON.parse(localStorage.getItem("inventory1"));
+// stuffValueParse1 = JSON.parse(localStorage.getItem("inventoryValue1"));
+// Modify money if this is a transaction
+// console.log(stuffParse1 + " " + stuffValueParse1);
+
+//Process for selling an item from inventory1 as well as its corosponding value. Don't use this until after intro sets inventory to empty array.
+// Check to see if user has enough money in local storage if this is a transaction
+// stuff1 = JSON.parse(localStorage.getItem("inventory1"));
+// stuffValue1 = JSON.parse(localStorage.getItem("inventoryValue1"));
+// stuff1.splice(stuff1.indexOf(item), 1);
+// stuffValue1.splice(stuffValue1.indexOf(item), 1);
+// localStorage.setItem("inventory1", JSON.stringify(stuff1));
+// localStorage.setItem("inventoryValue1", JSON.stringify(stuffValue1));
+// stuffParse1 = JSON.parse(localStorage.getItem("inventory1"));
+// stuffValueParse1 = JSON.parse(localStorage.getItem("inventoryValue1"));
+// Modify money if this is a transaction
+// console.log(stuffParse1 + " " + stuffValueParse1);
+
 //Max HP for each file
 let maxHP1 = localStorage.getItem("maxHP1");
 let maxHP2 = localStorage.getItem("maxHP2");
@@ -148,16 +227,15 @@ function startBalanced(){
   localStorage.setItem("defense1", "2");
   localStorage.setItem("magic1", "2");
   localStorage.setItem("dexterity1", "2");
-  maxHP1 = localStorage.getItem("maxHP1");
-  HP1 = localStorage.getItem("HP1");
-  maxMP1 = localStorage.getItem("maxMP1");
-  MP1 = localStorage.getItem("MP1");
-  maxSP1 = localStorage.getItem("maxSP1");
-  SP1  = localStorage.getItem("SP1");
-  attack1 = localStorage.getItem("attack1");
-  defense1 = localStorage.getItem("defense1");
-  magic1 = localStorage.getItem("magic1");
-  dexterity1 = localStorage.getItem("dexterity1");
+  localStorage.setItem("money1", "0");
+  localStorage.setItem("inventory1", JSON.stringify(stuff1));
+  localStorage.setItem("inventoryValue1", JSON.stringify(stuffValue1));
+  localStorage.setItem("weapon1", "Fists");
+  localStorage.setItem("weaponAtk1", "0");
+  localStorage.setItem("weaponValue1", "0");
+  localStorage.setItem("armor1", "Cloth");
+  localStorage.setItem("armorDef1", "0");
+  localStorage.setItem("armorValue1", "0");
   localStorage.setItem("intro1", true);
   }
   else if (fileNumber === "2"){
@@ -173,16 +251,15 @@ function startBalanced(){
     localStorage.setItem("defense2", "2");
     localStorage.setItem("magic2", "2");
     localStorage.setItem("dexterity2", "2");
-    maxHP2 = localStorage.getItem("maxHP2");
-    HP2 = localStorage.getItem("HP2");
-    maxMP2 = localStorage.getItem("maxMP2");
-    MP2 = localStorage.getItem("MP2");
-    maxSP2 = localStorage.getItem("maxSP2");
-    SP2  = localStorage.getItem("SP2");
-    attack2 = localStorage.getItem("attack2");
-    defense2 = localStorage.getItem("defense2");
-    magic2 = localStorage.getItem("magic2");
-    dexterity2 = localStorage.getItem("dexterity2");
+    localStorage.setItem("money2", "0");
+    localStorage.setItem("inventory2", JSON.stringify(stuff2));
+    localStorage.setItem("inventoryValue2", JSON.stringify(stuffValue2));
+    localStorage.setItem("weapon2", "Fists");
+    localStorage.setItem("weaponAtk2", "0");
+    localStorage.setItem("weaponValue2", "0");
+    localStorage.setItem("armor2", "Cloth");
+    localStorage.setItem("armorDef2", "0");
+    localStorage.setItem("armorValue2", "0");
     localStorage.setItem("intro2", true);
     }
   else if (fileNumber === "3"){
@@ -198,16 +275,15 @@ function startBalanced(){
     localStorage.setItem("defense3", "2");
     localStorage.setItem("magic3", "2");
     localStorage.setItem("dexterity3", "2");
-    maxHP3 = localStorage.getItem("maxHP3");
-    HP3 = localStorage.getItem("HP3");
-    maxMP3 = localStorage.getItem("maxMP3");
-    MP2 = localStorage.getItem("MP3");
-    maxSP3 = localStorage.getItem("maxSP3");
-    SP3  = localStorage.getItem("SP3");
-    attack3 = localStorage.getItem("attack3");
-    defense3 = localStorage.getItem("defense3");
-    magic3 = localStorage.getItem("magic3");
-    dexterity3 = localStorage.getItem("dexterity3");
+    localStorage.setItem("money3", "0");
+    localStorage.setItem("inventory3", JSON.stringify(stuff3));
+    localStorage.setItem("inventoryValue3", JSON.stringify(stuffValue3));
+    localStorage.setItem("weapon3", "Fists");
+    localStorage.setItem("weaponAtk3", "0");
+    localStorage.setItem("weaponValue3", "0");
+    localStorage.setItem("armor3", "Cloth");
+    localStorage.setItem("armorDef3", "0");
+    localStorage.setItem("armorValue3", "0");
     localStorage.setItem("intro3", true);
     };
     document.getElementById("confirmIntro").style.display="block";
@@ -229,16 +305,15 @@ function startAttack(){
   localStorage.setItem("defense1", "2");
   localStorage.setItem("magic1", "1");
   localStorage.setItem("dexterity1", "2");
-  maxHP1 = localStorage.getItem("maxHP1");
-  HP1 = localStorage.getItem("HP1");
-  maxMP1 = localStorage.getItem("maxMP1");
-  MP1 = localStorage.getItem("MP1");
-  maxSP1 = localStorage.getItem("maxSP1");
-  SP1  = localStorage.getItem("SP1");
-  attack1 = localStorage.getItem("attack1");
-  defense1 = localStorage.getItem("defense1");
-  magic1 = localStorage.getItem("magic1");
-  dexterity1 = localStorage.getItem("dexterity1");
+  localStorage.setItem("money1", "0");
+  localStorage.setItem("inventory1", JSON.stringify(stuff1));
+  localStorage.setItem("inventoryValue1", JSON.stringify(stuffValue1));
+  localStorage.setItem("weapon1", "Fists");
+  localStorage.setItem("weaponAtk1", "0");
+  localStorage.setItem("weaponValue1", "0");
+  localStorage.setItem("armor1", "Cloth");
+  localStorage.setItem("armorDef1", "0");
+  localStorage.setItem("armorValue1", "0");
   localStorage.setItem("intro1", true);
   }
   else if (fileNumber === "2"){
@@ -254,16 +329,15 @@ function startAttack(){
     localStorage.setItem("defense2", "2");
     localStorage.setItem("magic2", "1");
     localStorage.setItem("dexterity2", "2");
-    maxHP2 = localStorage.getItem("maxHP2");
-    HP2 = localStorage.getItem("HP2");
-    maxMP2 = localStorage.getItem("maxMP2");
-    MP2 = localStorage.getItem("MP2");
-    maxSP2 = localStorage.getItem("maxSP2");
-    SP2  = localStorage.getItem("SP2");
-    attack2 = localStorage.getItem("attack2");
-    defense2 = localStorage.getItem("defense2");
-    magic2 = localStorage.getItem("magic2");
-    dexterity2 = localStorage.getItem("dexterity2");
+    localStorage.setItem("money2", "0");
+    localStorage.setItem("inventory2", JSON.stringify(stuff2));
+    localStorage.setItem("inventoryValue2", JSON.stringify(stuffValue2));
+    localStorage.setItem("weapon2", "Fists");
+    localStorage.setItem("weaponAtk2", "0");
+    localStorage.setItem("weaponValue2", "0");
+    localStorage.setItem("armor2", "Cloth");
+    localStorage.setItem("armorDef2", "0");
+    localStorage.setItem("armorValue2", "0");
     localStorage.setItem("intro2", true);
     }
   else if (fileNumber === "3"){
@@ -279,16 +353,15 @@ function startAttack(){
     localStorage.setItem("defense3", "2");
     localStorage.setItem("magic3", "1");
     localStorage.setItem("dexterity3", "2");
-    maxHP3 = localStorage.getItem("maxHP3");
-    HP3 = localStorage.getItem("HP3");
-    maxMP3 = localStorage.getItem("maxMP3");
-    MP2 = localStorage.getItem("MP3");
-    maxSP3 = localStorage.getItem("maxSP3");
-    SP3  = localStorage.getItem("SP3");
-    attack3 = localStorage.getItem("attack3");
-    defense3 = localStorage.getItem("defense3");
-    magic3 = localStorage.getItem("magic3");
-    dexterity3 = localStorage.getItem("dexterity3");
+    localStorage.setItem("money3", "0");
+    localStorage.setItem("inventory3", JSON.stringify(stuff3));
+    localStorage.setItem("inventoryValue3", JSON.stringify(stuffValue3));
+    localStorage.setItem("weapon3", "Fists");
+    localStorage.setItem("weaponAtk3", "0");
+    localStorage.setItem("weaponValue3", "0");
+    localStorage.setItem("armor3", "Cloth");
+    localStorage.setItem("armorDef3", "0");
+    localStorage.setItem("armorValue3", "0");
     localStorage.setItem("intro3", true);
     };
     document.getElementById("confirmIntro").style.display="block";
@@ -310,16 +383,15 @@ function startDefense(){
   localStorage.setItem("defense1", "4");
   localStorage.setItem("magic1", "1");
   localStorage.setItem("dexterity1", "2");
-  maxHP1 = localStorage.getItem("maxHP1");
-  HP1 = localStorage.getItem("HP1");
-  maxMP1 = localStorage.getItem("maxMP1");
-  MP1 = localStorage.getItem("MP1");
-  maxSP1 = localStorage.getItem("maxSP1");
-  SP1  = localStorage.getItem("SP1");
-  attack1 = localStorage.getItem("attack1");
-  defense1 = localStorage.getItem("defense1");
-  magic1 = localStorage.getItem("magic1");
-  dexterity1 = localStorage.getItem("dexterity1");
+  localStorage.setItem("money1", "0");
+  localStorage.setItem("inventory1", JSON.stringify(stuff1));
+  localStorage.setItem("inventoryValue1", JSON.stringify(stuffValue1));
+  localStorage.setItem("weapon1", "Fists");
+  localStorage.setItem("weaponAtk1", "0");
+  localStorage.setItem("weaponValue1", "0");
+  localStorage.setItem("armor1", "Cloth");
+  localStorage.setItem("armorDef1", "0");
+  localStorage.setItem("armorValue1", "0");
   localStorage.setItem("intro1", true);
   }
   else if (fileNumber === "2"){
@@ -335,16 +407,15 @@ function startDefense(){
     localStorage.setItem("defense2", "4");
     localStorage.setItem("magic2", "1");
     localStorage.setItem("dexterity2", "2");
-    maxHP2 = localStorage.getItem("maxHP2");
-    HP2 = localStorage.getItem("HP2");
-    maxMP2 = localStorage.getItem("maxMP2");
-    MP2 = localStorage.getItem("MP2");
-    maxSP2 = localStorage.getItem("maxSP2");
-    SP2  = localStorage.getItem("SP2");
-    attack2 = localStorage.getItem("attack2");
-    defense2 = localStorage.getItem("defense2");
-    magic2 = localStorage.getItem("magic2");
-    dexterity2 = localStorage.getItem("dexterity2");
+    localStorage.setItem("money2", "0");
+    localStorage.setItem("inventory2", JSON.stringify(stuff2));
+    localStorage.setItem("inventoryValue2", JSON.stringify(stuffValue2));
+    localStorage.setItem("weapon2", "Fists");
+    localStorage.setItem("weaponAtk2", "0");
+    localStorage.setItem("weaponValue2", "0");
+    localStorage.setItem("armor2", "Cloth");
+    localStorage.setItem("armorDef2", "0");
+    localStorage.setItem("armorValue2", "0");
     localStorage.setItem("intro2", true);
     }
   else if (fileNumber === "3"){
@@ -360,16 +431,15 @@ function startDefense(){
     localStorage.setItem("defense3", "4");
     localStorage.setItem("magic3", "1");
     localStorage.setItem("dexterity3", "2");
-    maxHP3 = localStorage.getItem("maxHP3");
-    HP3 = localStorage.getItem("HP3");
-    maxMP3 = localStorage.getItem("maxMP3");
-    MP2 = localStorage.getItem("MP3");
-    maxSP3 = localStorage.getItem("maxSP3");
-    SP3  = localStorage.getItem("SP3");
-    attack3 = localStorage.getItem("attack3");
-    defense3 = localStorage.getItem("defense3");
-    magic3 = localStorage.getItem("magic3");
-    dexterity3 = localStorage.getItem("dexterity3");
+    localStorage.setItem("money3", "0");
+    localStorage.setItem("inventory3", JSON.stringify(stuff3));
+    localStorage.setItem("inventoryValue3", JSON.stringify(stuffValue3));
+    localStorage.setItem("weapon3", "Fists");
+    localStorage.setItem("weaponAtk3", "0");
+    localStorage.setItem("weaponValue3", "0");
+    localStorage.setItem("armor3", "Cloth");
+    localStorage.setItem("armorDef3", "0");
+    localStorage.setItem("armorValue3", "0");
     localStorage.setItem("intro3", true);
     };
     document.getElementById("confirmIntro").style.display="block";
@@ -391,16 +461,15 @@ function startMagic(){
   localStorage.setItem("defense1", "1");
   localStorage.setItem("magic1", "4");
   localStorage.setItem("dexterity1", "2");
-  maxHP1 = localStorage.getItem("maxHP1");
-  HP1 = localStorage.getItem("HP1");
-  maxMP1 = localStorage.getItem("maxMP1");
-  MP1 = localStorage.getItem("MP1");
-  maxSP1 = localStorage.getItem("maxSP1");
-  SP1  = localStorage.getItem("SP1");
-  attack1 = localStorage.getItem("attack1");
-  defense1 = localStorage.getItem("defense1");
-  magic1 = localStorage.getItem("magic1");
-  dexterity1 = localStorage.getItem("dexterity1");
+  localStorage.setItem("money1", "0");
+  localStorage.setItem("inventory1", JSON.stringify(stuff1));
+  localStorage.setItem("inventoryValue1", JSON.stringify(stuffValue1));
+  localStorage.setItem("weapon1", "Fists");
+  localStorage.setItem("weaponAtk1", "0");
+  localStorage.setItem("weaponValue1", "0");
+  localStorage.setItem("armor1", "Cloth");
+  localStorage.setItem("armorDef1", "0");
+  localStorage.setItem("armorValue1", "0");
   localStorage.setItem("intro1", true);
   }
   else if (fileNumber === "2"){
@@ -416,16 +485,15 @@ function startMagic(){
     localStorage.setItem("defense2", "1");
     localStorage.setItem("magic2", "4");
     localStorage.setItem("dexterity2", "2");
-    maxHP2 = localStorage.getItem("maxHP2");
-    HP2 = localStorage.getItem("HP2");
-    maxMP2 = localStorage.getItem("maxMP2");
-    MP2 = localStorage.getItem("MP2");
-    maxSP2 = localStorage.getItem("maxSP2");
-    SP2  = localStorage.getItem("SP2");
-    attack2 = localStorage.getItem("attack2");
-    defense2 = localStorage.getItem("defense2");
-    magic2 = localStorage.getItem("magic2");
-    dexterity2 = localStorage.getItem("dexterity2");
+    localStorage.setItem("money2", "0");
+    localStorage.setItem("inventory2", JSON.stringify(stuff2));
+    localStorage.setItem("inventoryValue2", JSON.stringify(stuffValue2));
+    localStorage.setItem("weapon2", "Fists");
+    localStorage.setItem("weaponAtk2", "0");
+    localStorage.setItem("weaponValue2", "0");
+    localStorage.setItem("armor2", "Cloth");
+    localStorage.setItem("armorDef2", "0");
+    localStorage.setItem("armorValue2", "0");
     localStorage.setItem("intro2", true);
     }
   else if (fileNumber === "3"){
@@ -441,16 +509,15 @@ function startMagic(){
     localStorage.setItem("defense3", "1");
     localStorage.setItem("magic3", "4");
     localStorage.setItem("dexterity3", "2");
-    maxHP3 = localStorage.getItem("maxHP3");
-    HP3 = localStorage.getItem("HP3");
-    maxMP3 = localStorage.getItem("maxMP3");
-    MP2 = localStorage.getItem("MP3");
-    maxSP3 = localStorage.getItem("maxSP3");
-    SP3  = localStorage.getItem("SP3");
-    attack3 = localStorage.getItem("attack3");
-    defense3 = localStorage.getItem("defense3");
-    magic3 = localStorage.getItem("magic3");
-    dexterity3 = localStorage.getItem("dexterity3");
+    localStorage.setItem("money3", "0");
+    localStorage.setItem("inventory3", JSON.stringify(stuff3));
+    localStorage.setItem("inventoryValue3", JSON.stringify(stuffValue3));
+    localStorage.setItem("weapon3", "Fists");
+    localStorage.setItem("weaponAtk3", "0");
+    localStorage.setItem("weaponValue3", "0");
+    localStorage.setItem("armor3", "Cloth");
+    localStorage.setItem("armorDef3", "0");
+    localStorage.setItem("armorValue3", "0");
     localStorage.setItem("intro3", true);
     };
     document.getElementById("confirmIntro").style.display="block";
@@ -463,3 +530,11 @@ function unsure(){
 function confirm(){
   window.location='game';
 };
+
+const array = [2, 5, 9, 11, 14, 17, 21];
+
+console.log(array);
+  array.splice(1, 1);
+
+// array = [2, 9]
+console.log(array); 
